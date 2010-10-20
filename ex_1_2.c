27da52d74e6f167fe583cff72dc748a81f964809
Daniel Näslund <daniel@longitudo.com>
2010-10-20 18:48:41 +0200

    Exercise 2.

---
+++ ex_1_2.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(int argc, char const* argv[])
+{
+  printf("Hello world\n");
+  printf("bell\a\n");
+  printf("random character\v");
+  printf("\\x1 \x1\n");
+  printf("\\o  \o\n");
+  return 0;
+}
