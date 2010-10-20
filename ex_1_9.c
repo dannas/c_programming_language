89929f1138bc756290b3a20109ff4bc1c97ef3ad
Daniel Näslund <daniel@longitudo.com>
2010-10-20 20:15:39 +0200

    Exercise 1.9.

---
+++ ex_1_9.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+#define SPACE 0
+#define NO_SPACE 1
+
+/**
+ * Substitue multi-spaces into single ones.
+ */
+int main(int argc, char const* argv[])
+{
+  int c;
+  int last_written = NO_SPACE;
+
+  while ((c = getchar()) != EOF)
+    {
+      if (last_written == SPACE)
+        {
+          if (c == ' ')
+            ;
+          else
+            {
+              putchar(c);
+              last_written = NO_SPACE;
+            }
+        }
+      else 
+        {
+          if (c == ' ')
+            {
+              putchar(c);
+              last_written = SPACE;
+            }
+          else
+            putchar(c);
+        }
+    }
+  return 0;
+}
