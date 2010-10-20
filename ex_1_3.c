7c40ab1c12c021d2909e1cb441cbb299a19456d7
Daniel Näslund <daniel@longitudo.com>
2010-10-20 18:55:49 +0200

    Exercise 3.

---
+++ ex_1_3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main(int argc, char const* argv[])
+{
+  float fahr, celsius;
+  int lower, upper, step;
+  int i;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  /* Print header */
+  printf("Fahrenheit Celsius\n");
+  for (i = 0; i < 80; i++)
+    putchar('=');
+  putchar('\n');
+
+  /* Print table */
+  fahr = lower;
+  while (fahr <= upper) {
+    celsius = (5.0 / 9.0) * (fahr - 32.0);
+    printf("%3.0f %6.1f\n", fahr, celsius);
+    fahr = fahr + step;
+  }
+  return 0;
+}
