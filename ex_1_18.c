594a221259f1485d4b3a8d57a94d0abf5f59acce
Daniel Näslund <daniel@longitudo.com>
2010-10-21 19:08:31 +0200

    Exercise 1.18 (the right one).

---
+++ ex_1_18.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+
+#define MAX_LENGTH 100
+
+int read_line(char s[], int lim)
+{
+  int i, c;
+
+  for (i = 0; i < lim -1 && (c = getchar()) != EOF && c != '\n'; i++) {
+    s[i] = c;
+  }
+
+  if (c == '\n') {
+    s[i] = '\n';
+    ++i;
+  }
+
+  s[i] = '\0';
+
+  return i;
+}
+
+int do_remove(char s[])
+{
+  int i = 0;
+
+  while (s[i] != '\n')
+    ++i;
+
+  --i;
+
+  while (i > 0 && (s[i] == ' ' || s[i] == '\t'))
+    --i;
+
+  if (i > 0) {
+    ++i;
+    s[i] = '\n';
+    ++i;
+    s[i] = '\0';
+  }
+
+  return i;
+}
+
+int main(int argc, char const* argv[])
+{
+  char line[MAX_LENGTH];
+  int start, n;
+
+  while (n = read_line(line, MAX_LENGTH)) {
+      if (do_remove(line) > 0) {
+         printf("%s", line); 
+        }
+    }
+
+  return 0;
+}
