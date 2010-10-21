f71845a1ac3d689a5f96b977fb38959352d27864
Daniel Näslund <daniel@longitudo.com>
2010-10-21 21:35:33 +0200

    Exercise 1.20.

---
+++ ex_1_20.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+#define TABINC 8
+
+/**
+ * Translate tabs to spaces, while preserving tabstops.
+ */
+int main(int argc, char const* argv[])
+{
+  int c, nb, pos = 1;
+
+  while ((c = getchar()) != EOF) {
+
+    if (c == '\t') {
+
+      nb = TABINC - (pos -1) % TABINC;
+      while (nb > 0) {
+        putchar(' ');
+        nb--;
+        pos++;
+      }
+    }
+
+    else if (c == '\n') {
+      putchar(c);
+      pos = 1;
+    }
+    else {
+      putchar(c);
+      ++pos;
+    }
+  }
+  
+  return 0;
+}
