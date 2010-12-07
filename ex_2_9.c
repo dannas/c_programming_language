8a15f602b107018bfe89250cea53a72743f69e8a
Daniel Näslund <daniel@longitudo.com>
2010-12-07 21:15:39 +0100

    Exercise 2.9.

---
+++ ex_2_9.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int
+bitcount(unsigned int x)
+{
+  int b;
+
+  for (b = 0; x; x &= x - 1)
+    b++;
+
+  return b;
+}
+
+int 
+main(int argc, char const* argv[])
+{
+  printf("bitcount=%d\n", bitcount(0x0F));
+  printf("bitcount=%d\n", bitcount(0x01));
+  return 0;
+}
